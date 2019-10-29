18242f307ac54c7255900213816a357d01ca02dd
singhstack <55691535+singhstack@users.noreply.github.com>
2019-10-29 18:19:15 +0530

    Update search_bing_api.py

--- search_bing_api.py
+++ search_bing_api.py
@@ -1,19 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+#I created a dataset for a neural network that recognizes Formula 1 drivers from the 2019-2020 Season.
+# Collectinhgd ~250 images each for 20 drivers.
+#**Spoiler Alert**the network found it difficult to distinguish Max Versrtappen (Red Bull) from Pier Gasly (Toro Rosso)
 from requests import exceptions
 import argparse
 import requests
 import cv2
 import os
 
-
-# In[2]:
-
-
+#You can either use command line to pass arguments or simply define them...
 '''# construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-q", "--query", required=True,
@@ -22,35 +16,21 @@ ap.add_argument("-o", "--output", required=True,
     help="path to output directory of images")
 args = vars(ap.parse_args())'''
 
-
-# In[8]:
-
-
 args={}
-args["query"]="Footballers"
+args["query"]="Lewis Hamilton"
 args["output"]=r"C:\Users\tarandeepsingh1\Downloads\Vision\Building Image Dataset using Bing API\Footballer Dataset"
-
-
-# In[13]:
-
-
-API_KEY="77fa602924364837802538708d19fb1a"
-MAX_RESULTS=250
-GROUP_SIZE=50
 
 
 # Enter the API Key obtained from Bing Cognitive Services.
 # I am limiting max number of results to 250 here and returning the max number of images per request by the Bing API to 50 total images.
+#(Although when training a network, each class should have atleast 1000 images)
 
 # Group size can be thought of as total number of results 'per page'
-
-# In[10]:
-
+API_KEY="77fa602924364837802538708d1XXXXXX"
+MAX_RESULTS=250
+GROUP_SIZE=50
 
 URL="https://api.cognitive.microsoft.com/bing/v7.0/images/search"
-
-
-# In[11]:
 
 
 # when attempting to download images from the web both the Python
@@ -60,9 +40,6 @@ URL="https://api.cognitive.microsoft.com/bing/v7.0/images/search"
 EXCEPTIONS = set([IOError, FileNotFoundError,
     exceptions.RequestException, exceptions.HTTPError,
     exceptions.ConnectionError, exceptions.Timeout])
-
-
-# In[16]:
 
 
 # store the search term in a convenience variable then set the
@@ -84,9 +61,6 @@ print("[INFO] {} total results for '{}'".format(estNumResults,term))
 
 # initialize the total number of images downloaded thus far
 total=0
-
-
-# In[19]:
 
 
 # loop over the estimated number of results in `GROUP_SIZE` groups
@@ -124,10 +98,3 @@ for offset in range(0, estNumResults, GROUP_SIZE):
             if type(e) in EXCEPTIONS:
                 print("[INFO] skipping: {}".format(v["contentUrl"]))
                 continue
-
-
-# In[ ]:
-
-
-
-
